43631de842a661a16efba495953f7fff6015edfa
sushimampfer <sushimampfer@duck.com>
2025-11-03 00:08:58 +0100

    removed size change

--- src/file.rs
+++ src/file.rs
@@ -1,25 +1,24 @@
 use crate::StreamError;
 use crate::{FrameData, VideoStream, VideoStreamIterator};
-use std::path::{Path, PathBuf};
 use std::fs;
 use std::io;
 use std::io::ErrorKind;
+use std::path::{Path, PathBuf};
 
 pub struct FileSource {
     source: PathBuf,
-    frame_size: (u32, u32),
 }
 
 impl FileSource {
-    pub fn new(source: &Path, frame_size: (u32, u32)) -> Result<Self, CaptureError> {
+    pub fn new(source: &Path) -> Result<Self, CaptureError> {
         let source = fs::canonicalize(source)?;
         if !source.exists() {
-            return Err(CaptureError::IoError(io::Error::new(ErrorKind::NotFound, "File not found")));
+            return Err(CaptureError::IoError(io::Error::new(
+                ErrorKind::NotFound,
+                "File not found",
+            )));
         }
-        Ok(Self {
-            source,
-            frame_size,
-        })
+        Ok(Self { source })
     }
 }
 
@@ -29,10 +28,8 @@ impl IntoIterator for FileSource {
 
     fn into_iter(self) -> Self::IntoIter {
         let pipeline_description = format!(
-            "uridecodebin uri=\"file://{}\" ! videoconvert ! videoscale ! capsfilter caps=\"video/x-raw, width={}, height={}\"",
-            self.source.to_string_lossy(),
-            self.frame_size.0,
-            self.frame_size.1
+            "uridecodebin uri=\"file://{}\" ! videoconvert ! video/x-raw,format=RGB",
+            self.source.to_string_lossy()
         );
         VideoStream::new(pipeline_description).into_iter()
     }
