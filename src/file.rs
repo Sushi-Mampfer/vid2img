a2c31e4a0507e68cdf76f698bad95941650d7373
sushimampfer <sushimampfer@duck.com>
2025-11-03 01:57:47 +0100

    more bugfixes

--- src/file.rs
+++ src/file.rs
@@ -28,7 +28,7 @@ impl IntoIterator for FileSource {
 
     fn into_iter(self) -> Self::IntoIter {
         let pipeline_description = format!(
-            "playbin uri=\"file://{}\" video-sink=\"videoconvert\"",
+            "uridecodebin uri=\"file://{}\" name=src ! videoconvert ! video/x-raw,format=RGB",
             self.source.to_string_lossy()
         );
         VideoStream::new(pipeline_description).into_iter()
